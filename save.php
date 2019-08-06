804ce647e325ff49f5f58a4c84f610897165cabb
ELIAS NGUMBI <32370330+eliasngumbi998@users.noreply.github.com>
2019-08-06 05:20:39 +0300

    Create save.php

---
+++ save.php
@@ -0,0 +1,11 @@
+<?php
+	require_once 'conn.php';
+ 
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$address = $_POST['address'];
+ 
+	mysqli_query($conn, "INSERT INTO `member` VALUES('', '$firstname', '$lastname', '$address')") or die(mysqli_error());
+ 
+	header("location: index.php");
+?>
